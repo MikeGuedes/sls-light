c831851d0c293d797948bd6edc66366eb54e66d7
guedesmichae@gmail.com <guedesmichae@gmail.com@b848ce51-dbbe-867e-e3a0-df40bc1fdd85>
2014-06-30 22:48:04 +0000

    [sls-light] Tela de Estoque >> Botão "Busca" (por código) > Finalizado.

--- trunk/SLS_2.8_JPA/src/telas/GUI_estoque.java
+++ trunk/SLS_2.8_JPA/src/telas/GUI_estoque.java
@@ -1,9 +1,23 @@
 package telas;
 
+import classes.Categoria;
+import classes.Produto;
+import funcoes.Rotinas;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 public class GUI_estoque extends javax.swing.JInternalFrame {
+
+	EntityManager gerenciador;
+	Query consulta;
+	List<Categoria>lista;
+	Rotinas rotina = new Rotinas();
+	Produto produto = new Produto();
 
 	public GUI_estoque() {
 		initComponents();
+		this.ListaCategorias();
 	}//CONSTRUTOR
 
 	@SuppressWarnings("unchecked")
@@ -11,19 +25,19 @@ public class GUI_estoque extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jComboBox1 = new javax.swing.JComboBox();
-        lbl_buscar = new javax.swing.JLabel();
-        lbl_ordenar = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox();
-        txt_nome = new javax.swing.JTextField();
-        btn_adicionar1 = new javax.swing.JButton();
+        box_categoria = new javax.swing.JComboBox();
+        lbl_categoria = new javax.swing.JLabel();
+        txt_codigo = new javax.swing.JTextField();
+        btn_busca_codigo = new javax.swing.JButton();
+        lbl_codigo = new javax.swing.JLabel();
+        btn_busca_categoria = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbl_itens = new javax.swing.JTable();
         jPanel3 = new javax.swing.JPanel();
-        lbl_buscar1 = new javax.swing.JLabel();
-        lbl_buscar2 = new javax.swing.JLabel();
-        lbl_buscar3 = new javax.swing.JLabel();
+        lbl_cod = new javax.swing.JLabel();
+        lbl_desc = new javax.swing.JLabel();
+        lbl_est = new javax.swing.JLabel();
         txt_nome1 = new javax.swing.JTextField();
         lbl_buscar4 = new javax.swing.JLabel();
         btn_adicionar = new javax.swing.JButton();
@@ -32,23 +46,32 @@ public class GUI_estoque extends javax.swing.JInternalFrame {
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
-        lbl_buscar.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        lbl_buscar.setForeground(new java.awt.Color(0, 102, 153));
-        lbl_buscar.setText("Categoria:");
+        lbl_categoria.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        lbl_categoria.setForeground(new java.awt.Color(0, 102, 153));
+        lbl_categoria.setText("Categoria:");
 
-        lbl_ordenar.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        lbl_ordenar.setForeground(new java.awt.Color(0, 102, 153));
-        lbl_ordenar.setText("Ordenar por:");
+        txt_codigo.setBackground(new java.awt.Color(255, 255, 204));
+        txt_codigo.setForeground(new java.awt.Color(102, 102, 102));
 
-        txt_nome.setBackground(new java.awt.Color(255, 255, 204));
-        txt_nome.setForeground(new java.awt.Color(102, 102, 102));
-
-        btn_adicionar1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        btn_adicionar1.setForeground(new java.awt.Color(102, 102, 102));
-        btn_adicionar1.setText("Buscar");
-        btn_adicionar1.addActionListener(new java.awt.event.ActionListener() {
+        btn_busca_codigo.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btn_busca_codigo.setForeground(new java.awt.Color(102, 102, 102));
+        btn_busca_codigo.setText("Buscar");
+        btn_busca_codigo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_adicionar1ActionPerformed(evt);
+                btn_busca_codigoActionPerformed(evt);
+            }
+        });
+
+        lbl_codigo.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        lbl_codigo.setForeground(new java.awt.Color(0, 102, 153));
+        lbl_codigo.setText("Código");
+
+        btn_busca_categoria.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btn_busca_categoria.setForeground(new java.awt.Color(102, 102, 102));
+        btn_busca_categoria.setText("Buscar");
+        btn_busca_categoria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_busca_categoriaActionPerformed(evt);
             }
         });
 
@@ -58,37 +81,34 @@ public class GUI_estoque extends javax.swing.JInternalFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbl_buscar)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbl_ordenar)
+                    .addComponent(lbl_categoria)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(box_categoria, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txt_nome, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btn_busca_categoria)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(txt_codigo, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btn_adicionar1)))
-                .addGap(0, 0, Short.MAX_VALUE))
+                        .addComponent(btn_busca_codigo))
+                    .addComponent(lbl_codigo)))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, 0)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbl_categoria)
+                    .addComponent(lbl_codigo))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txt_codigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(lbl_buscar)
-                        .addGap(32, 32, 32))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(lbl_ordenar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txt_nome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(btn_adicionar1)
-                                .addContainerGap())
-                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addComponent(btn_busca_codigo)
+                        .addContainerGap())
+                    .addComponent(box_categoria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn_busca_categoria)))
         );
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
@@ -122,17 +142,17 @@ public class GUI_estoque extends javax.swing.JInternalFrame {
 
         jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
-        lbl_buscar1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        lbl_buscar1.setForeground(new java.awt.Color(0, 102, 153));
-        lbl_buscar1.setText("Código");
+        lbl_cod.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        lbl_cod.setForeground(new java.awt.Color(0, 102, 153));
+        lbl_cod.setText("Código");
 
-        lbl_buscar2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        lbl_buscar2.setForeground(new java.awt.Color(0, 102, 153));
-        lbl_buscar2.setText("Descrição:");
+        lbl_desc.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        lbl_desc.setForeground(new java.awt.Color(0, 102, 153));
+        lbl_desc.setText("Descrição:");
 
-        lbl_buscar3.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        lbl_buscar3.setForeground(new java.awt.Color(0, 102, 153));
-        lbl_buscar3.setText("Produtos em estoque:");
+        lbl_est.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        lbl_est.setForeground(new java.awt.Color(0, 102, 153));
+        lbl_est.setText("Produtos em estoque:");
 
         txt_nome1.setBackground(new java.awt.Color(255, 255, 204));
         txt_nome1.setForeground(new java.awt.Color(102, 102, 102));
@@ -158,9 +178,9 @@ public class GUI_estoque extends javax.swing.JInternalFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbl_buscar2)
-                    .addComponent(lbl_buscar3)
-                    .addComponent(lbl_buscar1))
+                    .addComponent(lbl_desc)
+                    .addComponent(lbl_est)
+                    .addComponent(lbl_cod))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(lbl_buscar4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -173,14 +193,14 @@ public class GUI_estoque extends javax.swing.JInternalFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_buscar1)
+                    .addComponent(lbl_cod)
                     .addComponent(lbl_buscar4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addComponent(lbl_buscar2)
+                        .addComponent(lbl_desc)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lbl_buscar3))
+                        .addComponent(lbl_est))
                     .addComponent(txt_nome1)))
             .addComponent(btn_adicionar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
@@ -228,29 +248,56 @@ public class GUI_estoque extends javax.swing.JInternalFrame {
 
     }//GEN-LAST:event_btn_adicionarActionPerformed
 
-    private void btn_adicionar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_adicionar1ActionPerformed
+    private void btn_busca_codigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_busca_codigoActionPerformed
+        
+		gerenciador = rotina.Conectar();
+		
+		int codigo = Integer.parseInt(this.txt_codigo.getText());
+		produto = (Produto)rotina.Localiza(gerenciador, codigo, "Produto");
+		
+		this.lbl_cod.setText("Código: "+produto.getId());
+		this.lbl_desc.setText("Descrição: "+produto.getNome());
+		this.lbl_est.setText("Produtos em estoque: "+produto.getEstoque());
+		
+    }//GEN-LAST:event_btn_busca_codigoActionPerformed
+
+    private void btn_busca_categoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_busca_categoriaActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btn_adicionar1ActionPerformed
+    }//GEN-LAST:event_btn_busca_categoriaActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox box_categoria;
     private javax.swing.JButton btn_adicionar;
-    private javax.swing.JButton btn_adicionar1;
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JComboBox jComboBox2;
+    private javax.swing.JButton btn_busca_categoria;
+    private javax.swing.JButton btn_busca_codigo;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JLabel lbl_buscar;
-    private javax.swing.JLabel lbl_buscar1;
-    private javax.swing.JLabel lbl_buscar2;
-    private javax.swing.JLabel lbl_buscar3;
     private javax.swing.JLabel lbl_buscar4;
-    private javax.swing.JLabel lbl_ordenar;
+    private javax.swing.JLabel lbl_categoria;
+    private javax.swing.JLabel lbl_cod;
+    private javax.swing.JLabel lbl_codigo;
+    private javax.swing.JLabel lbl_desc;
+    private javax.swing.JLabel lbl_est;
     private javax.swing.JTable tbl_itens;
-    private javax.swing.JTextField txt_nome;
+    private javax.swing.JTextField txt_codigo;
     private javax.swing.JTextField txt_nome1;
     // End of variables declaration//GEN-END:variables
+	
+	public void ListaCategorias(){
+
+		this.box_categoria.addItem("Todas");
+		
+                  gerenciador = rotina.Conectar();
+                  consulta = gerenciador.createQuery("select c from Categoria c");
+                  lista = consulta.getResultList();
+                  for (int i=0 ; i<lista.size() ; i++) {
+                           this.box_categoria.addItem(lista.get(i).getNome());
+                  }//FOR
+                  rotina.Fechar(gerenciador);
+         
+         }//LISTA CATEGORIAS
 	
 }//CLASS
